012a0b03701b061e7debcdbe4866a4c6ee29541c
Patrick Vu <patrick95117@gmail.com>
2015-11-19 07:56:25 -0800

    RecyclerView in nav drawer (broken adapter)

--- app/src/main/java/com/example/michael/deliveryapp/Home.java
+++ app/src/main/java/com/example/michael/deliveryapp/Home.java
@@ -5,13 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
+    RecyclerView mDrawerRecycler;
+    RelativeLayout mDrawerPane;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    ArrayList<NavigationItem> mNavItems = new ArrayList<NavigationItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +37,35 @@ public class Home extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         NavigationDrawerFragment fragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.home_fragment_navigation_drawer);
-        fragment.setUp(R.id.home_fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        fragment.setUp(R.id.home_fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
 
+        mNavItems.add(new NavigationItem("Item 1", R.drawable.example_item));
+        mNavItems.add(new NavigationItem("Item 2", R.drawable.example_item));
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerRecycler = (RecyclerView) findViewById(R.id.navRecycler);
+        DrawerRecyclerAdapter adapter = new DrawerRecyclerAdapter(Home.this.getApplicationContext(), mNavItems);
+        mDrawerRecycler.setAdapter(adapter);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,6 +87,10 @@ public class Home extends AppCompatActivity {
             return true;
         }
 
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        
         return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/michael/deliveryapp/NavigationItem.java
@@ -0,0 +1,14 @@
+package com.example.michael.deliveryapp;
+
+/**
+ * Created by Patrick on 11/12/2015.
+ */
+public class NavigationItem {
+    String mName;
+    int mIcon;
+
+    public NavigationItem(String name, int icon) {
+        mName = name;
+        mIcon = icon;
+    }
+}
