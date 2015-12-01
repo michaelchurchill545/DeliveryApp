7941069dc35b9693df4f7932b878d964c46bdc6c
michaelchurchill545 <michael.churchill@sjsu.edu>
2015-12-01 11:29:58 -0800

    NEW FEATURES: -Factory Method Created For Choose Item -Nav Bar items, when clicked, actually go to Home and Choose Item -Images hopefully imported onto git hub this time

---
+++ app/src/main/java/com/example/michael/deliveryapp/GetUserCallback.java
@@ -0,0 +1,19 @@
+package com.example.michael.deliveryapp;
+
+/**
+ * Created by Patrick Balingit on 11/21/2015.
+ */
+interface GetUserCallback {
+    void done(User returnedUser);
+}
+
+
+
+
+
+
+
+
+
+
+
